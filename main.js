ca530a9801a186686772236fd540298c5266fcf2
Phoebe Greig <Phoebe.Greig@gmail.com>
2016-09-22 14:50:02 +0100

    created weekly climate status

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 _getWeatherData(35, 139);
 
+var weeklyDailyStatus = [];
 function _getWeatherData(lat, lon){
 		fetch('http://api.openweathermap.org/data/2.5/forecast/city?lat=' + lat + '&lon=' + lon + '&APPID=73d75b47f73a3e213e08d898e79c7c2d&cnt=7').then(
 		function(productResponse){
 			productResponse.json().then(function(productData){
-				console.log(productData.list);
+        weeklyClimates(productData.list);
 			});
 		}
 	)
@@ -12,26 +13,43 @@ function _getWeatherData(lat, lon){
 
 function Weather(){
   this.WEATHERTYPES = {
-    "shower rain": "wet",
-    "rain": "wet",
-    "thunderstorm": "wet",
-    "snow": "wet"
+    "Shower rain": "wet",
+    "Rain": "wet",
+    "Thunderstorm": "wet",
+    "Snow": "wet"
   };
   this.COLD_THRESHOLD = 12;
   this.MILD_THRESHOLD = 20;
 }
 
+var weather = new Weather();
 
-var WEATHERTYPES = {
-  "shower rain": "wet",
-  "rain": "wet",
-  "thunderstorm": "wet",
-  "snow": "wet"
+
+var weatherStatus = [];
+
+function weeklyClimates(weeklyWeatherArr) {
+  var arrayLength = weeklyWeatherArr.length;
+  for (var i = 0; i < arrayLength; i++) {
+  weeklyDailyStatus.push(getDailyClimates(weeklyWeatherArr[i]));
+  weatherStatus = [];
+  }
 }
 
-new Weather();
-// cold: [12],
-// mild: [12,20],
-// warm: [20],
-//
-// var TEMPTYPES =
+function getDailyClimates(dailyWeatherStats) {
+  findWeatherType(dailyWeatherStats.weather[0].main);
+  findClimateType(dailyWeatherStats.main.temp);
+  return weatherStatus;
+}
+
+function findWeatherType(weatherDescription) {
+  return weatherStatus.push(weather.WEATHERTYPES[weatherDescription]);
+}
+
+function findClimateType(avTemp) {
+  if (avTemp <= weather.COLD_THRESHOLD){
+    weatherStatus.push("cold");
+  } else if (avTemp > weather.COLD_THRESHOLD && avTemp <= weather.MILD_THRESHOLD) {
+    weatherStatus.push("mild");
+  } else
+  weatherStatus.push("warm");
+}
