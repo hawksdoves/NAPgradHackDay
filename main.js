b9a2446806df741fdd15605c53e417ec37a092d3
Phoebe Greig <Phoebe.Greig@gmail.com>
2016-09-22 13:07:33 +0100

    working on grouping weather categories

--- main.js
+++ main.js
@@ -4,8 +4,34 @@ function _getWeatherData(lat, lon){
 		fetch('http://api.openweathermap.org/data/2.5/forecast/city?lat=' + lat + '&lon=' + lon + '&APPID=73d75b47f73a3e213e08d898e79c7c2d&cnt=7').then(
 		function(productResponse){
 			productResponse.json().then(function(productData){
-				console.log(productData.list);					
+				console.log(productData.list);
 			});
 		}
-	)	
+	)
 }
+
+function Weather(){
+  this.WEATHERTYPES = {
+    "shower rain": "wet",
+    "rain": "wet",
+    "thunderstorm": "wet",
+    "snow": "wet"
+  };
+  this.COLD_THRESHOLD = 12;
+  this.MILD_THRESHOLD = 20;
+}
+
+
+var WEATHERTYPES = {
+  "shower rain": "wet",
+  "rain": "wet",
+  "thunderstorm": "wet",
+  "snow": "wet"
+}
+
+new Weather();
+// cold: [12],
+// mild: [12,20],
+// warm: [20],
+//
+// var TEMPTYPES =
